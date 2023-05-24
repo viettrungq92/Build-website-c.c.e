f441f32aec69e6f788d7af6a3fece49f938ecb7b
viettrungq92 <viettrungq92@gmail.com>
2023-05-24 21:00:20 +0700

    code head

---
+++ intro.php
@@ -0,0 +1,35 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Course Template">
+    <meta name="keywords" content="tin học thiếu nhi, tin học văn phòng, chứng chỉ quốc tế , 
+    tin học cơ khí, tin học xây dựng , đồ hoạ xử lý ảnh , thiết kế web , hoạ viên bim , lập trình iots ,
+    kỹ thuật lập trình , ktv công nghệ in 3d, html">
+    <title>Trung Tâm Kỹ Thuật Điện Toán-TRƯỜNG ĐẠI HỌC BÁCH KHOA</title>
+
+    <!-- import bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    
+    <!-- font awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css" />
+    
+    <!-- font google  -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- owl.carousel css  -->
+    <link rel="stylesheet" href="./CSS/owl.carousel.css" />
+    <link rel="stylesheet" href="./CSS/owl.theme.default.css" />
+
+    <!-- main css  -->
+    <link rel="stylesheet" href="./css/index.css" />
+</head>
+<body>
+    
+</body>
